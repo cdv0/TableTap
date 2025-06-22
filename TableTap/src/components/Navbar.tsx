3a3a587c01efb1ec542f4e73a5836837c257ed1f
erichongg <honggeric@gmail.com>
2025-06-22 15:30:01 -0700

    implement global sidebar for navbar

--- TableTap/src/components/Navbar.tsx
+++ TableTap/src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 interface Props {
   heading: string;
+  onToggleSidebar: () => void;
 }
 
-const Navbar = ({ heading }: Props) => {
+const Navbar = ({ heading, onToggleSidebar }: Props) => {
   const today = new Date().toLocaleDateString("en-US", {
     weekday: "short",
     month: "short",
@@ -10,10 +11,21 @@ const Navbar = ({ heading }: Props) => {
   });
 
   return (
-    <nav className="navbar" style={{ backgroundColor: "#484848" }}>
-      <div className="container-fluid d-flex justify-content-between text-white">
-        <span className="navbar-brand mb-0 h1 text-white">{heading}</span>
-        <span className="navbar-brand mb-0 h1 text-white">{today}</span>
+    <nav className="navbar navbar-expand-lg pb-0 pt-0 navbar-dark" style={{ backgroundColor: "#484848" }}>
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        
+        <div className="d-flex align-items-center">
+          <button
+            className="btn text-white fs-2 me-2"
+            onClick={onToggleSidebar}
+            aria-label="Toggle sidebar"
+          >
+            ☰
+          </button>
+          <span className="navbar-brand mb-0 h1 fs-4">{heading}</span>
+        </div>
+
+        <span className="navbar-brand mb-0 h1 fs-4">{today}</span>
       </div>
     </nav>
   );
