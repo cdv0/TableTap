37c56ad84a82531fe78ca0a272b270cf6512bc9b
Cathleen Vu <cathleendvu@gmail.com>
2025-07-01 23:28:53 -0700

    Remove files from previous commit

--- TableTap/src/pages/Assets.tsx
+++ TableTap/src/pages/Assets.tsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
 import Navbar from "../components/features/employee/global/Navbar";
-import AssetNavigationSidebar from "../components/AssetNavigationSidebar";
-import OverlaySidebarShell from "../components/OverlaySidebarShell";
 import Sidebar from "../components/features/employee/global/Sidebar";
 
 const Assets = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [showOverlaySidebar, setShowOverlaySidebar] = useState(false);
 
   return (
     <div className="d-flex flex-column" style={{ height: "100vh" }}>
@@ -15,32 +12,6 @@ const Assets = () => {
         onToggleSidebar={() => setSidebarOpen(!sidebarOpen)}
       />
       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-
-      <div className="d-flex flex-row flex-grow-1 overflow-hidden">
-        <AssetNavigationSidebar />
-
-        <div>
-          <h1>Assets</h1>
-          {/*TEST SIDEBAR OVERLAY*/}
-          <button onClick={() => setShowOverlaySidebar(true)}>
-            Open sidebar
-          </button>
-        </div>
-      </div>
-
-      {/* TEST: Overlay Sidebar*/}
-      {showOverlaySidebar && (
-        <OverlaySidebarShell
-          title="Menu item"
-          group="Add modifier group"
-          onClose={() => {
-            setShowOverlaySidebar(false);
-          }}
-          onSave={() => {}}
-        >
-          <p>Test</p>
-        </OverlaySidebarShell>
-      )}
     </div>
   );
 };
