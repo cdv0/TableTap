93ffa470c7097524639ac7361f568ba7b94fa0d8
Cathleen Vu <cathleendvu@gmail.com>
2025-07-21 00:25:10 -0700

    Create and style Order screen category navigation button

--- TableTap/src/pages/Orders.tsx
+++ TableTap/src/pages/Orders.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import CategoryNavButton from '../components/features/employee/order/CategoryNavButton';
 
 function Orders() {
   return (
     <div>
         <h1>Order</h1>
+        {/* TEST */}
+        <CategoryNavButton text="Pho" color="blue"></CategoryNavButton>
     </div>
   )
 }
