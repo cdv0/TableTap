296e369848d804e156a71d1267e25816337469d9
Cathleen Vu <cathleendvu@gmail.com>
2025-07-20 23:24:32 -0700

    Add route to Order screen

--- TableTap/src/App.tsx
+++ TableTap/src/App.tsx
@@ -7,6 +7,7 @@ import AdminRequests from "./pages/AdminRequests";
 import CreateOrg from "./pages/CreateOrg";
 import EmployeeDashboard from "./pages/EmployeeDashboard";
 import Tables from "./pages/Tables";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/admin-dashboard/requests" element={<AdminRequests />} />
         <Route path="/assets" element={<Assets />} />
         <Route path="/tables" element={<Tables />} />
+        <Route path="/tables/orders" element={<Orders />} />
       </Routes>
     </Router>
   );
